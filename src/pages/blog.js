bb06034e045c8029d5277c3701bd931e16fefc0f
Pete Traversa <peter.traversa@gmail.com>
2019-03-07 12:53:17 -0500

    background added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,7 +3,7 @@ import React from 'react';
 class BlogPage extends React.Component {
   render() {
     return (
-      <section id="one" className="main style1 major">
+      <section id="two" className="main style2">
         <h1>Hello world from the blog page</h1>
       </section>
     )
