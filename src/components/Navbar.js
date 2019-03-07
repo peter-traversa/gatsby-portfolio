d2d0904b729588f47a427c69fa5086d6cae2c838
Pete Traversa <peter.traversa@gmail.com>
2019-03-07 14:03:39 -0500

    working on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import { Link } from 'gatsby';
 class Navbar extends React.Component {
   render() {
     return (
-      <section id="header">
-        <Link to="/">Home</Link>
-        <Link to="/blog">Blog</Link>
-      </section>
+      <>
+        <section id="navbar">
+          <Link to="/">Home</Link>{"  "}
+          <Link to="/blog">Blog</Link>
+        </section>
+      </>
     )
   }
 }
