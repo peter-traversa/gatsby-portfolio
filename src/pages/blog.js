cadcd4dfcd354b4976f3d1d4786f537939e86b0e
Pete Traversa <peter.traversa@gmail.com>
2019-03-07 15:58:07 -0500

    fetch not working yet, but map set up

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,8 +5,24 @@ import { Card } from '@material-ui/core';
 import { CardHeader } from '@material-ui/core';
 
 class BlogPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      blogs: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@peter.traversa')
+    .then(res => res.json())
+    .then(blogs => this.setState({blogs}))
+  }
+
+  handleCardClick(e) {
+    console.log(e.target)
+  }
+
   render() {
-    const pageTitle = "Peter Traversa Blog Posts"
     return (
       <>
         <Navbar />
@@ -14,7 +30,11 @@ class BlogPage extends React.Component {
             <h1>Recent Blogs</h1>
           </div>
           <div id="blog-page">
-
+            {this.state.blogs.items.map((blog, idx) =>
+              <Card onClick={this.handleCardClick} >
+                <CardHeader id={idx} title={blog.title} />
+              </Card>
+            )}
           </div>
         <Footer />
       </>
