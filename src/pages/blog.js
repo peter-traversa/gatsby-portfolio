612a883aebdd1787b2317e1398503295cfa7a350
Pete Traversa <peter.traversa@gmail.com>
2019-03-15 10:55:18 -0400

    grid working on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import BlogCard from '../components/BlogCard';
-import Grid from '@material-ui/core/grid';
 
 class BlogPage extends React.Component {
   constructor() {
@@ -25,14 +24,12 @@ class BlogPage extends React.Component {
           <div id="blog-title">
             <h1>Recent Blogs</h1>
           </div>
-          <div id="blog-page">
-            <Grid container spacing={16} justify={'space-around'}>
-              {!this.state.blogs ? <h1>Cannot display recent blogs</h1> : this.state.blogs.map((blog, idx) =>
-                <Grid item key={idx} >
-                  <BlogCard blog={blog} />
-                </Grid>
-              )}
-            </Grid>
+          <div id="blog-page" className='grid-wrapper' >
+            {!this.state.blogs ? <h1>Cannot display recent blogs</h1> : this.state.blogs.map((blog, idx) =>
+              <div className='col-3' key={idx} >
+                <BlogCard key={idx} blog={blog} />
+              </div>
+            )}
           </div>
         <Footer />
       </>
