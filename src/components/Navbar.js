8eccd07a8ea3e07a645f983572c411c35c807922
Pete Traversa <peter.traversa@gmail.com>
2019-03-25 15:33:23 -0400

    new link added to recent projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ class Navbar extends React.Component {
       <>
         <section id="navbar">
           <Link to="/" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Home</Link>{"  "}
+          <a href="#three" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Recent Projects</a>
           <Link to="/blog" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Blog</Link>{"  "}
           <Link to="/resume" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Resume</Link>
         </section>
