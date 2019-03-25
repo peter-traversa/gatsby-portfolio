31aa35650519eb1a864b54fe9213dc1f031bb004
Pete Traversa <peter.traversa@gmail.com>
2019-03-25 15:55:58 -0400

    broke it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { navigate } from '@reach/router';
 
 class Navbar extends React.Component {
   render() {
+    const handleRecentProjects = () => {
+      navigate('/');
+      navigate('#three');
+    }    
     return (
       <>
         <section id="navbar">
           <Link to="/" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Home</Link>{"  "}
-          <a href="#three" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Recent Projects</a>
+          <a onClick={handleRecentProjects} style={{ color: "white" }} >Recent Projects</a>{"  "}
           <Link to="/blog" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Blog</Link>{"  "}
           <Link to="/resume" style={{ color: "white" }} activeStyle={{ color: "gray"}}>Resume</Link>
         </section>
